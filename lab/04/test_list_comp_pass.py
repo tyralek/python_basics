37fda75e659af13616f908232a1c4c145af24465
Rafal Ozieblo <rafalo@cadence.com>
2017-04-11 20:30:34 +0100

    list comrehension

---
+++ lab/04/test_list_comp_pass.py
@@ -0,0 +1,58 @@
+'''
+@author: rozieblo
+
+List comprehension exercise
+'''
+import unittest
+import time
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_list_comp_simple(self):
+        items = [-2, 3, 0, 2.5, -1, 6]
+        squared = []
+        even = []
+        doubled_odds = []
+
+        self.assertEqual(squared, [4, 9, 0, 6.25, 1, 36])
+        self.assertEqual(even, [-2, 0, 6])
+        self.assertEqual(doubled_odds, [6, -2])
+
+    def test_list_comp_time(self):
+        len = 5000000
+        start = time.time()
+        noprimes = []
+        items = range(len)
+        for i in range(2, int(len ** 0.5)):
+            for j in range(i * 2, len, i):
+                noprimes.append(j)
+        time_loop = time.time() - start
+        print('Time with loop: {}'.format(time_loop))
+        start = time.time()
+        noprimes_comp = [ 0 ]
+        time_comp = time.time() - start
+        print('Time with comprehension: {}'.format(time_comp))
+        #self.assertEqual(noprimes, noprimes_comp)
+        self.assertGreater(time_loop, time_comp)
+
+    def test_list_comp_string(self):
+        words = "Litwo! Ojczyzno moja! Ty jesteś jak zdrowie".split()
+        result = []
+        self.assertEqual(result, \
+            [['LITWO!', 'litwo!', 6],
+             ['OJCZYZNO', 'ojczyzno', 8],
+             ['MOJA!', 'moja!', 5],
+             ['TY', 'ty', 2],
+             ['JESTEŚ', 'jesteś', 6],
+             ['JAK', 'jak', 3],
+             ['ZDROWIE', 'zdrowie', 7]])
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
