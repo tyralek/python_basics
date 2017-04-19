c9b52280a23ca6e2f3bd1c21d4370e3a595dee8f
Rafal Ozieblo <rafalo@cadence.com>
2017-04-19 08:28:55 +0100

    pylint fix

--- lab/tic_tac_toe/test_tic_tac_toe.py
+++ lab/tic_tac_toe/test_tic_tac_toe.py
@@ -4,7 +4,8 @@
 Tic tac toe test
 '''
 import unittest
-from tic_tac_toe import tic_tac_toe
+from tic_tac_toe import TicTacToe
+
 
 class Test(unittest.TestCase):
 
@@ -15,52 +16,52 @@ class Test(unittest.TestCase):
         pass
 
     def test_ttt_put(self):
-        ttt = tic_tac_toe()
-        self.assertEqual(ttt.board, [[0,0,0],[0,0,0],[0,0,0]])
+        ttt = TicTacToe()
+        self.assertEqual(ttt.board, [[0, 0, 0], [0, 0, 0], [0, 0, 0]])
         self.assertEqual(ttt.now_move(), 'x')
         self.assertTrue(ttt.put('b2'))
         self.assertEqual(ttt.now_move(), 'o')
-        self.assertEqual(ttt.board, [[0,0,0],[0,'x',0],[0,0,0]])
+        self.assertEqual(ttt.board, [[0, 0, 0], [0, 'x', 0], [0, 0, 0]])
         self.assertTrue(ttt.put('a1'))
         self.assertEqual(ttt.now_move(), 'x')
-        self.assertEqual(ttt.board, [['o',0,0],[0,'x',0],[0,0,0]])
+        self.assertEqual(ttt.board, [['o', 0, 0], [0, 'x', 0], [0, 0, 0]])
         self.assertTrue(ttt.put('c3'))
         self.assertEqual(ttt.now_move(), 'o')
-        self.assertEqual(ttt.board, [['o',0,0],[0,'x',0],[0,0,'x']])
+        self.assertEqual(ttt.board, [['o', 0, 0], [0, 'x', 0], [0, 0, 'x']])
         self.assertTrue(ttt.put('a3'))
         self.assertEqual(ttt.now_move(), 'x')
-        self.assertEqual(ttt.board, [['o',0,0],[0,'x',0],['o',0,'x']])
+        self.assertEqual(ttt.board, [['o', 0, 0], [0, 'x', 0], ['o', 0, 'x']])
         self.assertFalse(ttt.put('d1'))
         self.assertEqual(ttt.now_move(), 'x')
-        self.assertEqual(ttt.board, [['o',0,0],[0,'x',0],['o',0,'x']])
+        self.assertEqual(ttt.board, [['o', 0, 0], [0, 'x', 0], ['o', 0, 'x']])
         self.assertFalse(ttt.put('a1'))
         self.assertEqual(ttt.now_move(), 'x')
-        self.assertEqual(ttt.board, [['o',0,0],[0,'x',0],['o',0,'x']])
+        self.assertEqual(ttt.board, [['o', 0, 0], [0, 'x', 0], ['o', 0, 'x']])
         self.assertFalse(ttt.put('a1'))
         self.assertEqual(ttt.now_move(), 'x')
-        self.assertEqual(ttt.board, [['o',0,0],[0,'x',0],['o',0,'x']])
+        self.assertEqual(ttt.board, [['o', 0, 0], [0, 'x', 0], ['o', 0, 'x']])
         self.assertFalse(ttt.put('b2'))
         self.assertEqual(ttt.now_move(), 'x')
-        self.assertEqual(ttt.board, [['o',0,0],[0,'x',0],['o',0,'x']])
+        self.assertEqual(ttt.board, [['o', 0, 0], [0, 'x', 0], ['o', 0, 'x']])
         self.assertFalse(ttt.put('a0'))
         self.assertEqual(ttt.now_move(), 'x')
-        self.assertEqual(ttt.board, [['o',0,0],[0,'x',0],['o',0,'x']])
+        self.assertEqual(ttt.board, [['o', 0, 0], [0, 'x', 0], ['o', 0, 'x']])
         self.assertFalse(ttt.put('3'))
         self.assertEqual(ttt.now_move(), 'x')
-        self.assertEqual(ttt.board, [['o',0,0],[0,'x',0],['o',0,'x']])
+        self.assertEqual(ttt.board, [['o', 0, 0], [0, 'x', 0], ['o', 0, 'x']])
 
     def test_ttt_clear(self):
-        ttt = tic_tac_toe()
-        self.assertEqual(ttt.board, [[0,0,0],[0,0,0],[0,0,0]])
+        ttt = TicTacToe()
+        self.assertEqual(ttt.board, [[0, 0, 0], [0, 0, 0], [0, 0, 0]])
         ttt.put('b2')
-        self.assertEqual(ttt.board, [[0,0,0],[0,'x',0],[0,0,0]])
+        self.assertEqual(ttt.board, [[0, 0, 0], [0, 'x', 0], [0, 0, 0]])
         ttt.put('a1')
-        self.assertEqual(ttt.board, [['o',0,0],[0,'x',0],[0,0,0]])
+        self.assertEqual(ttt.board, [['o', 0, 0], [0, 'x', 0], [0, 0, 0]])
         ttt.clear()
-        self.assertEqual(ttt.board, [[0,0,0],[0,0,0],[0,0,0]])
+        self.assertEqual(ttt.board, [[0, 0, 0], [0, 0, 0], [0, 0, 0]])
 
     def test_ttt_winner_x_column(self):
-        ttt = tic_tac_toe()
+        ttt = TicTacToe()
         self.assertEqual(ttt.winner_is(), 0)
         ttt.put('c2')
         self.assertEqual(ttt.winner_is(), 0)
@@ -74,10 +75,11 @@ class Test(unittest.TestCase):
         self.assertEqual(ttt.winner_is(), 'x')
         self.assertFalse(ttt.put('b3'))
         self.assertEqual(ttt.winner_is(), 'x')
-        self.assertEqual(ttt.board, [[0,'o','x'],[0,'o','x'],[0,0,'x']])
+        self.assertEqual(ttt.board, [[0, 'o', 'x'], \
+                         [0, 'o', 'x'], [0, 0, 'x']])
 
     def test_ttt_winner_x_row(self):
-        ttt = tic_tac_toe()
+        ttt = TicTacToe()
         self.assertEqual(ttt.winner_is(), 0)
         ttt.put('c2')
         self.assertEqual(ttt.winner_is(), 0)
@@ -91,10 +93,11 @@ class Test(unittest.TestCase):
         self.assertEqual(ttt.winner_is(), 'x')
         self.assertFalse(ttt.put('a1'))
         self.assertEqual(ttt.winner_is(), 'x')
-        self.assertEqual(ttt.board, [[0,'o','o'],['x','x','x'],[0,0,0]])
+        self.assertEqual(ttt.board, [[0, 'o', 'o'], \
+                         ['x', 'x', 'x'], [0, 0, 0]])
 
     def test_ttt_winner_x_diagonal(self):
-        ttt = tic_tac_toe()
+        ttt = TicTacToe()
         self.assertEqual(ttt.winner_is(), 0)
         ttt.put('c3')
         self.assertEqual(ttt.winner_is(), 0)
@@ -108,10 +111,11 @@ class Test(unittest.TestCase):
         self.assertEqual(ttt.winner_is(), 'x')
         self.assertFalse(ttt.put('a1'))
         self.assertEqual(ttt.winner_is(), 'x')
-        self.assertEqual(ttt.board, [['x','o','o'],[0,'x',0],[0, 0, 'x']])
+        self.assertEqual(
+            ttt.board, [['x', 'o', 'o'], [0, 'x', 0], [0, 0, 'x']])
 
     def test_ttt_winner_o_column(self):
-        ttt = tic_tac_toe()
+        ttt = TicTacToe()
         self.assertEqual(ttt.winner_is(), 0)
         ttt.put('a1')
         self.assertEqual(ttt.winner_is(), 0)
@@ -127,10 +131,11 @@ class Test(unittest.TestCase):
         self.assertEqual(ttt.winner_is(), 'o')
         self.assertFalse(ttt.put('a2'))
         self.assertEqual(ttt.winner_is(), 'o')
-        self.assertEqual(ttt.board, [['x','o','x'],[0,'o',0],['x','o',0]])
+        self.assertEqual(ttt.board, [['x', 'o', 'x'], \
+                         [0, 'o', 0], ['x', 'o', 0]])
 
     def test_ttt_winner_o_row(self):
-        ttt = tic_tac_toe()
+        ttt = TicTacToe()
         self.assertEqual(ttt.winner_is(), 0)
         ttt.put('a1')
         self.assertEqual(ttt.winner_is(), 0)
@@ -146,10 +151,11 @@ class Test(unittest.TestCase):
         self.assertEqual(ttt.winner_is(), 'o')
         self.assertFalse(ttt.put('b1'))
         self.assertEqual(ttt.winner_is(), 'o')
-        self.assertEqual(ttt.board, [['x',0,'x'],['o','o','o'],[0,0,'x']])
+        self.assertEqual(ttt.board, [['x', 0, 'x'], \
+                         ['o', 'o', 'o'], [0, 0, 'x']])
 
     def test_ttt_winner_o_diagonal(self):
-        ttt = tic_tac_toe()
+        ttt = TicTacToe()
         self.assertEqual(ttt.winner_is(), 0)
         ttt.put('a2')
         self.assertEqual(ttt.winner_is(), 0)
@@ -165,7 +171,9 @@ class Test(unittest.TestCase):
         self.assertEqual(ttt.winner_is(), 'o')
         self.assertFalse(ttt.put('a1'))
         self.assertEqual(ttt.winner_is(), 'o')
-        self.assertEqual(ttt.board, [['x',0,'o'],['x','o',0],['o',0,'x']])
+        self.assertEqual(ttt.board, [['x', 0, 'o'], \
+                         ['x', 'o', 0], ['o', 0, 'x']])
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
--- lab/tic_tac_toe/tic_tac_toe.py
+++ lab/tic_tac_toe/tic_tac_toe.py
@@ -4,23 +4,35 @@
 Tic tac toe class
 '''
 
-class tic_tac_toe:
+
+class TicTacToe:
+    '''
+    Tic tac toe class
+    '''
     def __init__(self):
-        self.__board = [[0 for x in range(3)] for y in range(3)]
-        self.__x = 1
-        self.__winner = False
+        self.__x = 0
+        self.__winner = 0
+        self.clear()
 
     @property
     def board(self):
+        '''
+        Get board.
+        '''
         return self.__board
 
-    def __letter_to_column(self, letter):
+    @staticmethod
+    def __letter_to_column(letter):
         return ord(letter) - ord('a')
 
-    def __number_to_row(self, number):
+    @staticmethod
+    def __number_to_row(number):
         return ord(number) - ord('1')
 
     def put(self, field):
+        '''
+        put 'x' or 'o' to the board
+        '''
         if field[0] not in 'abc':
             return False
         if field[1] not in '123':
@@ -28,7 +40,7 @@ class tic_tac_toe:
         if self.__winner != 0:
             return False
 
-        column  = self.__letter_to_column(field[0])
+        column = self.__letter_to_column(field[0])
         row = self.__number_to_row(field[1])
         if self.__board[row][column] != 0:
             return False
@@ -55,14 +67,14 @@ class tic_tac_toe:
         return 0
 
     def __check_row(self, row):
-        r = list(set(self.__board[row]))
-        if len(r) == 1 and r[0] != 0:
-            return r[0]
+        set_row = list(set(self.__board[row]))
+        if len(set_row) == 1 and set_row[0] != 0:
+            return set_row[0]
         return 0
 
     def __check_diagonal(self):
         diag1 = list(set([self.__board[i][i] for i in range(3)]))
-        diag2 = list(set([self.__board[i][2-i] for i in range(3)]))
+        diag2 = list(set([self.__board[i][2 - i] for i in range(3)]))
 
         if len(diag1) == 1 and diag1[0] != 0:
             return diag1[0]
@@ -71,18 +83,32 @@ class tic_tac_toe:
         return 0
 
     def clear(self):
-        self.__board = [[0 for x in range(3)] for y in range(3)]
+        '''
+        Clear board and all markers. Reset game.
+        '''
+        self.__board = [[0 for _ in range(3)] for _ in range(3)]
         self.__winner = 0
+        self.__x = 1
 
     def winner_is(self):
+        '''
+        Who is the winner.
+        '''
         return self.__winner
 
     def now_move(self):
+        '''
+        Whos move is now.
+        '''
         if self.__x == 0:
             return 'o'
         return 'x'
 
+
 class Display:
+    '''
+    Display class, it shows current game status.
+    '''
     def __init__(self):
         self.__display = \
             "  a   b   c\n" \
@@ -100,26 +126,38 @@ class Display:
                     disp[col * 25 + row * 4 + 14] = ttt.board[col][row]
         print(''.join(disp))
 
-    def show(self, ttt = 0):
-        if ttt == 0:
-            print(self.__display)
+    def show(self, tic_tac_toe = None):
+        '''
+        Show current game status.
+        :param tic_tac_toe: TicTacToe object to show or 0 for empty board
+        :type tic_tac_toe: TicTacToe or None
+        '''
+        if tic_tac_toe:
+            self.__show_ttt(tic_tac_toe)
         else:
-            self.__show_ttt(ttt)
+            print(self.__display)
 
-if __name__ == "__main__":
-    my_ttt = tic_tac_toe()
+def main():
+    '''
+    main function for game.
+    '''
+    my_ttt = TicTacToe()
     my_display = Display()
     my_display.show()
     move_cnt = 0
-    while (my_ttt.winner_is() == 0 and move_cnt < 9):
-        move = input("Choose move for {} (a1 - c3): ".format(my_ttt.now_move()))
+    while my_ttt.winner_is() == 0 and move_cnt < 9:
+        move = input(
+            "Choose move for {} (a1 - c3): ".format(my_ttt.now_move()))
         print("\n")
         if my_ttt.put(move):
             my_display.show(my_ttt)
             move_cnt += 1
         else:
             print("You can't put {} in {}\n".format(move, my_ttt.now_move()))
-    if (my_ttt.winner_is()):
+    if my_ttt.winner_is():
         print("The winner is: {}\n".format(my_ttt.winner_is()))
     else:
         print("No one is the winner.\n")
+
+if __name__ == "__main__":
+    main()
